4ae38d68f386383e323e601f83ff8f307a32fc83
alicee <avega2003@yandex.ru>
2025-05-04 19:24:27 +0300

    logging idea added

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse
+import shutil
+import sys
+import subprocess
+import multiprocessing
+import threading
+import queue
+import time
+import os
 
 
 DEFAULT_REPORT_INTERVAL = 5
 DEFAULT_LOG_FILE = "sysbench_cpu_report.log"
-SYSBENCH_COMMAND = "/usr/lib/sysbench"
+PATH_TO_SYSBENCH = None
 
+def log_writer():
+    pass
 
 def main():
     
@@ -40,11 +50,35 @@ def main():
     )
     parser.add_argument(
         "--graph",
-        action="store_true", # Флаг, не требует значения
+        action="store_true", 
     )
 
     args = parser.parse_args()
+    
+    #print(args)
 
+    PATH_TO_SYSBENCH = shutil.which('sysbench')
+    
+    if not PATH_TO_SYSBENCH:
+        print(f"Error: '{PATH_TO_SYSBENCH}' not found. install it", file=sys.stderr)
+        sys.exit(1)
+    else:
+        print(f'sysbench found - {PATH_TO_SYSBENCH}')
+    
+    print(f"Starting {args.num_threads} parallel sysbench with time {args.time} seconds")
+    print(f"Reports every {args.report_interval} seconds")
+    print(f"Logging to {args.log_file}")
+
+
+    # очередь для сообщений
+    output_queue = multiprocessing.Queue()
+
+    # поток для записи логов
+    log_thread = threading.Thread(target=log_writer, args=(output_queue, args.log_file))
+    log_thread.start()
 
 if __name__ == "__main__":
     main()
+    
+    # ./main.py --num-threads=3 --time=60
+    # ./main.py --num-threads=1 --time=600
